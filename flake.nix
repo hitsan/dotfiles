dcec27b8c7eae83ab599eb8af788f2e66f546278
hitsan <soledewa2828@gmail.com>
2025-06-16 22:42:53 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nix-ld = {
-      url = "github:Mic92/nix-ld";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    xremap.url = "github:xremap/nix-flake";
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-ld, xremap }:
+  outputs = { self, nixpkgs, home-manager }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -22,22 +17,9 @@
     home = "/home/${user}";
     root = builtins.toString ./.;
     home_path = "${root}/home-manager";
-    modules_path = "${root}/modules";
-    hosts_path = "${root}/hosts";
     shell = "zsh";
   in
   {
-    nixosConfigurations = {
-      spica = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit user home nix-ld modules_path xremap;
-        };
-
-        modules = [ ./hosts/spica ];
-      };
-    };
-
     homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       extraSpecialArgs = { inherit user home shell home_path; };
