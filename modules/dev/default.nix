5ed2a490c7aee4c7fbfddf575ecd214c6ff40640
hitsan <soledewa2828@gmail.com>
2025-06-18 01:40:11 +0900

    remove keychain

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -6,11 +6,6 @@
       enableBashIntegration = true;
       nix-direnv.enable = true;
     };
-    keychain = {
-      enable = true;
-      agents = [ "ssh" ];
-      keys = [ "id_ed25519" ];
-    };
   };
   home.packages = with pkgs; [
     just
