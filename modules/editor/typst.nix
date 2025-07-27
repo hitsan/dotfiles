d6b05de774517c355a443d287ff6340771d09880
hitsan <soledewa2828@gmail.com>
2025-07-27 17:34:04 +0900

    add fonts

--- modules/editor/typst.nix
+++ modules/editor/typst.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     typst
     # Japanese fonts for proper PDF rendering
@@ -11,31 +12,4 @@
     source-han-serif
     source-han-sans
   ];
-
-  # Enable font configuration for proper CJK font handling
-  fonts.fontconfig.enable = true;
-  
-  # Explicit font configuration
-  home.file.".config/fontconfig/fonts.conf".text = ''
-    <?xml version="1.0"?>
-    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-    <fontconfig>
-      <alias>
-        <family>serif</family>
-        <prefer>
-          <family>Noto Serif CJK JP</family>
-          <family>Source Han Serif</family>
-          <family>IPAMincho</family>
-        </prefer>
-      </alias>
-      <alias>
-        <family>sans-serif</family>
-        <prefer>
-          <family>Noto Sans CJK JP</family>
-          <family>Source Han Sans</family>
-          <family>IPAGothic</family>
-        </prefer>
-      </alias>
-    </fontconfig>
-  '';
 }
