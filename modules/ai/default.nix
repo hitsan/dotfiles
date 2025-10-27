1f818c7df5b77944c03c249af432b0a695d7db36
hitsan <15902694+hitsan@users.noreply.github.com>
2025-10-27 19:42:39 +0900

    mv

--- modules/ai/default.nix
+++ modules/ai/default.nix
@@ -1,24 +1,5 @@
-{ pkgs, shell, ... }:
+{ ... }:
 {
-  programs = {
-    direnv = {
-      enable = true;
-      enableBashIntegration = true;
-      nix-direnv.enable = true;
-    };
-  };
-  home.packages = with pkgs; [
-    devbox
-    glow
-    just
-    mise
-  ];
-  programs.${shell} = {
-    shellAliases = {
-      glow = "glow -p -w $(tput cols)";
-    };
-  };
-
   imports = [
     ./claude
     ./gemini
--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,23 @@
-{ ... }:
+{ pkgs, shell, ... }:
 {
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
+  home.packages = with pkgs; [
+    devbox
+    glow
+    just
+  ];
+  programs.${shell} = {
+    shellAliases = {
+      glow = "glow -p -w $(tput cols)";
+    };
+  };
+
   imports = [
     ./shell
     ./zellij
