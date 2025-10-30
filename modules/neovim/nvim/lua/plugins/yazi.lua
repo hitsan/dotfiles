380f910303094d3674ad705205c6430aff084d38
hitsan <15902694+hitsan@users.noreply.github.com>
2025-10-30 12:03:17 +0900

    update key bind

--- modules/neovim/nvim/lua/plugins/yazi.lua
+++ modules/neovim/nvim/lua/plugins/yazi.lua
@@ -17,26 +17,28 @@ return {
       desc = "Open yazi in current working directory",
     },
   },
-  opts = {
-    -- Enable mouse support
-    enable_mouse_support = true,
+  config = function()
+    require("yazi").setup({
+      -- Enable mouse support
+      enable_mouse_support = true,
 
-    -- Open yazi instead of netrw
-    open_for_directories = false,
+      -- Open yazi instead of netrw
+      open_for_directories = false,
 
-    -- Keymaps to be used inside yazi
-    keymaps = {
-      show_help = '<f1>',
-      open_file_in_vertical_split = "<c-x>",
-      open_file_in_horizontal_split = "<c-h>",
-      open_file_in_tab = "<c-t>",
-      grep_in_directory = "<c-s>",
-      copy_relative_path_to_selected_files = "<c-y>",
-    },
+      -- Keymaps to be used inside yazi
+      keymaps = {
+        show_help = '<f1>',
+        open_file_in_vertical_split = "<c-x>",
+        open_file_in_horizontal_split = false, -- Disabled
+        open_file_in_tab = "<c-t>",
+        grep_in_directory = "<c-s>",
+        copy_relative_path_to_selected_files = "<c-y>",
+      },
 
-    -- Floating window settings
-    floating_window_scaling_factor = 0.9,
-    yazi_floating_window_winblend = 0,
-    yazi_floating_window_border = "rounded",
-  },
+      -- Floating window settings
+      floating_window_scaling_factor = 0.9,
+      yazi_floating_window_winblend = 0,
+      yazi_floating_window_border = "rounded",
+    })
+  end,
 }
