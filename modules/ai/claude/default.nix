63c126f6ecb075b7d6a8f0eb12d0f21acec9c51e
hitsan <15902694+hitsan@users.noreply.github.com>
2026-02-08 17:56:43 +0900

    fix path

--- modules/ai/claude/default.nix
+++ modules/ai/claude/default.nix
@@ -12,4 +12,6 @@
   home.file.".claude/CLAUDE.md".source = "${modules}/ai/claude/template/CLAUDE.md";
   home.file.".claude/agents".source = "${modules}/ai/claude/template/agents";
   home.file.".claude/skills/design".source = "${modules}/ai/claude/template/skills/design";
+  home.file.".claude/skills/arch".source = "${modules}/ai/claude/template/skills/arch";
+  home.file.".claude/skills/modeling".source = "${modules}/ai/claude/template/skills/modeling";
 }
