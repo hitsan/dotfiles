e632b4ec1609813e11f2b7ff6ea740998b8b0850
hitsan <15902694+hitsan@users.noreply.github.com>
2026-02-13 22:43:20 +0900

    add cmd

--- modules/git/ghq.nix
+++ modules/git/ghq.nix
@@ -1,6 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, shell, ... }:
 {
   home.packages = with pkgs; [
     ghq
   ];
+  programs.${shell} = {
+    shellAliases = {
+      cg = "z $(ghq list -p | fzf)";
+    };
+  };
 }
