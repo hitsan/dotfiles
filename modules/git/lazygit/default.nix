8d62d17c193cd23f6b965b89178083e5f7da9d0e
hitsan <15902694+hitsan@users.noreply.github.com>
2026-02-17 18:18:50 +0900

    remove aliase

--- modules/git/lazygit/default.nix
+++ modules/git/lazygit/default.nix
@@ -3,9 +3,6 @@
   programs.lazygit = {
     enable = true;
   };
-  programs.${shell}.shellAliases = {
-    lazg = "lazygit";
-  };
   home.file.".config/lazygit/config.yml" = {
     source = "${modules}/git/lazygit/config.yml";
   };
